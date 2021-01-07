eec5559048ce5db95809e5b2994523656b13a63f
Michelle Brownlee <michelle@orbwave.com>
2021-01-07 17:01:14 -0600

    loop to mock up saving db as csv

---
+++ functions.php
@@ -0,0 +1,42 @@
+<?php
+  include_once 'db_sql.php';
+
+    if(isset($_POST["Export"])){
+     
+      header('Content-Type: text/csv; charset=utf-8');  
+      header('Content-Disposition: attachment; filename=exported.csv');  
+      $output = fopen("exported.csv", "w");  
+      fputcsv($output, array('id', 'name', 'SSN'));  
+      $query = "SELECT id, name, SSN from pretendw2 ORDER BY id DESC";  
+      $result = mysqli_query($init, $query);  
+    //   $delimiter = ",";
+    //   $enclosure = '"';
+
+      while($row = mysqli_fetch_assoc($result))  
+      {  
+        fputcsv($output, $row, $delimiter = ",", $enclosure = "'");  
+      }  
+      fclose($output); 
+//     $value = mysqli_query($init, $query);   
+//     $row = mysqli_fetch_assoc($value);
+//     function encodeFunc($value) {
+//        ///remove any ESCAPED double quotes within string.
+//        $value = str_replace('\\"','"',$value);
+//        //then force escape these same double quotes And Any UNESCAPED Ones.
+//        $value = str_replace('"','\"',$value);
+//        //force wrap value in quotes and return
+//        return '"'.$value.'"';
+//    }
+   
+//    $fp = fopen("exported.csv", 'w');
+//    foreach($value as $row){
+//        fputs($fp, implode(",", array_map("encodeFunc", $row))."\r\n");
+//    }
+//    fclose($fp);
+//  }
+//  function encodeFunc($value) {
+//     return "\"$value\"";
+// }
+}
+// fputcsv($handler, array_map(encodeFunc, $array), ',', chr(0));
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<body>
+
+    <div class="container">
+            <form action="functions.php" method="post" name="upload_excel" enctype="multipart/form-data">
+                    <input type="submit" name="Export" class="btn" value="export to excel" />
+    </div>
+    <style>
+    .btn {
+        background-color: orange;
+        color: black;
+    }
+    .btn:hover {
+        background-color: lime;
+    }
+</style>
+</body>
